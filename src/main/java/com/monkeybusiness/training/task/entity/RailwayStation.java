6143d83d230835d57559024e933ca8afeb320ba8
KinBi <kinbizaval@gmail.com>
2020-11-20 17:03:45 +0300

    Commit

---
+++ src/main/java/com/monkeybusiness/training/task/entity/RailwayStation.java
@@ -0,0 +1,46 @@
+package com.monkeybusiness.training.task.entity;
+
+import java.util.Arrays;
+
+public class RailwayStation {
+  private Train[] trains;
+
+  public RailwayStation() {
+  }
+
+  public RailwayStation(Train[] trains) {
+    this.trains = trains;
+  }
+
+  public Train[] getTrains() {
+    return trains.clone();
+  }
+
+  public void setTrains(Train[] trains) {
+    this.trains = trains;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    RailwayStation that = (RailwayStation) o;
+
+    // Probably incorrect - comparing Object[] arrays with Arrays.equals
+    return Arrays.equals(trains, that.trains);
+  }
+
+  @Override
+  public int hashCode() {
+    return Arrays.hashCode(trains);
+  }
+
+  @Override
+  public String toString() {
+    final StringBuilder sb = new StringBuilder("RailvayStation{");
+    sb.append("trains=").append(Arrays.toString(trains));
+    sb.append('}');
+    return sb.toString();
+  }
+}
